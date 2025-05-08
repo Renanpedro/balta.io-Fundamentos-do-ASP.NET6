9c227c2c6566521cd2ba04c829a74e384ba8f45a
Renan Silva <contato.renan.silva@outlook.com>
2025-05-07 22:20:38 -0300

    Finalizado o modulo MVC

--- MVC/Todo/obj/Debug/net9.0/Todo.AssemblyInfo.cs
+++ MVC/Todo/obj/Debug/net9.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1c0b1074a405958e4cb06c186b22d761618a961")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b8c9898f3e2d94eb8562c0d75c48781ce4ca720")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
