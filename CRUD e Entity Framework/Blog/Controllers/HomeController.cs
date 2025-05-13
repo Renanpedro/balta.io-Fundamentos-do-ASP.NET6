a917f3dfb262420ce5aa006e2e3d8ae8e8dc71a9
Renan Silva <contato.renan.silva@outlook.com>
2025-05-12 23:12:51 -0300

    continuacao modulo crud e entity framework

---
+++ CRUD e Entity Framework/Blog/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// Health Check - Checagem de saude é um metodo para mostrar se api está ativa ou não
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
